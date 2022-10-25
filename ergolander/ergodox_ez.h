ae8aee99988e0d1126e8881a48bc3ead8e4be50a
darknessblade <darknessbladers@gmail.com>
2022-10-25 19:35:42 +0200

    Update ergodox_ez.h

--- ergolander/ergodox_ez.h
+++ ergolander/ergodox_ez.h
@@ -22,108 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-
-#if defined(KEYBOARD_ergodox_ez_glow)
-#    include "glow.h"
-#elif defined(KEYBOARD_ergodox_ez_shine)
-#    include "shine.h"
-#endif
-
-// I2C aliases and register addresses (see "mcp23018.md")
-#define I2C_ADDR        0b0100000
-#define I2C_ADDR_WRITE  ( (I2C_ADDR<<1) | I2C_WRITE )
-#define I2C_ADDR_READ   ( (I2C_ADDR<<1) | I2C_READ  )
-#define IODIRA          0x00            // i/o direction register
-#define IODIRB          0x01
-#define GPPUA           0x0C            // GPIO pull-up resistor register
-#define GPPUB           0x0D
-#define GPIOA           0x12            // general purpose i/o port register (write modifies OLAT)
-#define GPIOB           0x13
-#define OLATA           0x14            // output latch register
-#define OLATB           0x15
-
-extern i2c_status_t mcp23018_status;
-#define ERGODOX_EZ_I2C_TIMEOUT 100
-
-void init_ergodox(void);
-void ergodox_blink_all_leds(void);
-uint8_t init_mcp23018(void);
-uint8_t ergodox_left_leds_update(void);
-
-#ifndef LED_BRIGHTNESS_LO
-#define LED_BRIGHTNESS_LO       15
-#endif
-#ifndef LED_BRIGHTNESS_HI
-#define LED_BRIGHTNESS_HI       255
-#endif
-
-
-inline void ergodox_board_led_on(void)      { DDRD |=  (1<<6); PORTD |=  (1<<6); }
-inline void ergodox_right_led_1_on(void)    { DDRB |=  (1<<5); PORTB |=  (1<<5); }
-inline void ergodox_right_led_2_on(void)    { DDRB |=  (1<<6); PORTB |=  (1<<6); }
-inline void ergodox_right_led_3_on(void)    { DDRB |=  (1<<7); PORTB |=  (1<<7); }
-inline void ergodox_right_led_on(uint8_t led) { DDRB |= (1<<(led+4)); PORTB |= (1<<(led+4)); }
-
-inline void ergodox_board_led_off(void)     { DDRD &= ~(1<<6); PORTD &= ~(1<<6); }
-inline void ergodox_right_led_1_off(void)   { DDRB &= ~(1<<5); PORTB &= ~(1<<5); }
-inline void ergodox_right_led_2_off(void)   { DDRB &= ~(1<<6); PORTB &= ~(1<<6); }
-inline void ergodox_right_led_3_off(void)   { DDRB &= ~(1<<7); PORTB &= ~(1<<7); }
-inline void ergodox_right_led_off(uint8_t led) { DDRB &= ~(1<<(led+4)); PORTB &= ~(1<<(led+4)); }
-
-#ifdef LEFT_LEDS
-bool ergodox_left_led_1;
-bool ergodox_left_led_2;
-bool ergodox_left_led_3;
-
-inline void ergodox_left_led_1_on(void)    { ergodox_left_led_1 = 1; }
-inline void ergodox_left_led_2_on(void)    { ergodox_left_led_2 = 1; }
-inline void ergodox_left_led_3_on(void)    { ergodox_left_led_3 = 1; }
-
-inline void ergodox_left_led_1_off(void)    { ergodox_left_led_1 = 0; }
-inline void ergodox_left_led_2_off(void)    { ergodox_left_led_2 = 0; }
-inline void ergodox_left_led_3_off(void)    { ergodox_left_led_3 = 0; }
-#endif // LEFT_LEDS
-
-inline void ergodox_led_all_on(void) {
-    ergodox_board_led_on();
-    ergodox_right_led_1_on();
-    ergodox_right_led_2_on();
-    ergodox_right_led_3_on();
-#ifdef LEFT_LEDS
-    ergodox_left_led_1_on();
-    ergodox_left_led_2_on();
-    ergodox_left_led_3_on();
-#endif // LEFT_LEDS
-}
-
-inline void ergodox_led_all_off(void)
-{
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-#ifdef LEFT_LEDS
-    ergodox_left_led_1_off();
-    ergodox_left_led_2_off();
-    ergodox_left_led_3_off();
-#endif // LEFT_LEDS
-}
-
-inline void ergodox_right_led_1_set(uint8_t n)    { OCR1A = n; }
-inline void ergodox_right_led_2_set(uint8_t n)    { OCR1B = n; }
-inline void ergodox_right_led_3_set(uint8_t n)    { OCR1C = n; }
-inline void ergodox_right_led_set(uint8_t led, uint8_t n)  {
-    (led == 1) ? (OCR1A = n) :
-    (led == 2) ? (OCR1B = n) :
-                 (OCR1C = n);
-}
-
-inline void ergodox_led_all_set(uint8_t n) {
-    ergodox_right_led_1_set(n);
-    ergodox_right_led_2_set(n);
-    ergodox_right_led_3_set(n);
-}
-
 enum ergodox_ez_keycodes {
     LED_LEVEL = SAFE_RANGE,
     TOGGLE_LAYER_COLOR,
@@ -158,7 +56,7 @@ extern keyboard_config_t keyboard_config;
     k40,k41,k42,k43,k44,                                        \
                             k55,k56,                            \
                             k54,k53,                            \
-							k52,k51,                            \
+			    k52,k51,                            \
                                                                 \
         k07,k08,k09,k0A,k0B,k0C,k0D,                            \
         k17,k18,k19,k1A,k1B,k1C,k1D,                            \
@@ -167,7 +65,7 @@ extern keyboard_config_t keyboard_config;
                 k49,k4A,k4B,k4C,k4D,                            \
     k57,k58,                                                    \
     k59,k5C,                                                    \
-    k5B,k5A )                                              		\
+    k5B,k5A )                                              	\
                                                                 \
    /* matrix positions */                                       \
    {                                                            \
