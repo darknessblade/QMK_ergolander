da33a6e1e189bb8d19d32720d24b360f6512a5b1
darknessblade <darknessbladers@gmail.com>
2022-10-27 10:57:00 +0200

    Add files via upload
    rev3

---
+++ ergolander3/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+/*rp2040 settings*/
+#define RP2040_FLASH_W25Q080
+
+#define VIAL_KEYBOARD_UID {0xD1, 0x47, 0xEF, 0x04, 0xFA, 0x4D, 0xD3, 0x8C}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1}
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1} 
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
